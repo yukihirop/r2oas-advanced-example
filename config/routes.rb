373e396a8d922c2be4214408f775750100b22d2d
yukihirop <te108186@gmail.com>
2019-10-13 16:22:01 +0900

    bundle exec rails g scaffold Api/V2/CustomPost status:string content:string

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   namespace :api do
+    namespace :v2 do
+      resources :custom_posts
+    end
+  end
+  namespace :api do
     namespace :v1 do
       resources :accounts
     end
